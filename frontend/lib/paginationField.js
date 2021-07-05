744b87e5012865b2170397522e6fadf2b5c44cab
rlycrvns <rlycrvns@gmail.com>
2021-07-05 11:08:40 -0700

    add: pagination field lib

---
+++ frontend/lib/paginationField.js
@@ -0,0 +1,58 @@
+import { PAGINATION_QUERY } from '../components/Pagination';
+
+export default function paginationField() {
+  return {
+    keyArgs: false, // tells apollo we will take care of everything
+    read(existing = [], { args, cache }) {
+      const { skip, first } = args;
+
+      // read the number of items on the page from the cache
+      const data = cache.readQuery({ query: PAGINATION_QUERY });
+      const count = data?._allProductsMeta?.count;
+      const page = skip / first + 1;
+      const pages = Math.ceil(count / first);
+
+      // check if we have existing items
+      const items = existing.slice(skip, skip + first).filter((x) => x);
+      // If
+      // There are items
+      // AND there aren't enough items to satisfy how many were requested
+      // AND we are on the last page
+      // THEN JUST SEND IT
+      if (items.length && items.length !== first && page === pages) {
+        return items;
+      }
+
+      if (items.length !== first) {
+        // we don't have any items, we must go to the network to fetch
+        return false;
+      }
+
+      // if there are items, return them from the cache, don't go to network
+      if (items.length) {
+        // console.log(
+        //   `There are ${items.length} items in the cache! Sending them to Apollo`
+        // );
+        return items;
+      }
+
+      return false; // fallback to network
+
+      // first thing it foes is ask the read function for those items
+      // We can either do one of two things:
+      // First things we can do is return the items because they are already in the cache
+      // The other thing we can do is to return false from here, (network request)
+    },
+    merge(existing, incoming, { args }) {
+      // this runs when the apollo client comes back from the network with out product
+      const { skip, first } = args;
+      console.log(`Merging items from the network ${incoming.length}`);
+      const merged = existing ? existing.slice(0) : [];
+      for (let i = skip; i < skip + incoming.length; i += 1) {
+        merged[i] = incoming[i - skip];
+      }
+      // finally we return the merged items from the cache
+      return merged;
+    },
+  };
+}
